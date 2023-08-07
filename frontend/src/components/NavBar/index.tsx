b8359c39cea2707d09963a7790213b6f63df4a8f
Richard Brayan <richardbrayan2009@gmail.com>
2023-08-07 13:48:18 -0300

    NavBar Created

---
+++ frontend/src/components/NavBar/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-md bg-primary main-nav">
+      <div className="container-fluid">
+        <h4 className="text-secondary">BDS vendas</h4>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
